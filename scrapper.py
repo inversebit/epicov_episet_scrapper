be5e13c3023b9994e6575ac9c871a104654ccdc8
Inversebit <3485012+inversebit@users.noreply.github.com>
2022-06-21 20:11:41 +0200

    Automate id fetching

--- scrapper.py
+++ scrapper.py
@@ -3,34 +3,40 @@ import json
 import time
 from bs4 import BeautifulSoup
 
-with open("./codes.json", 'r') as j:
-     ids = json.loads(j.read())
-
 with open('results.csv', 'a') as the_file:
-    for elem in ids["ids"]:
 
-        time.sleep(1)
+    main_page = requests.get("https://epicov.org/epi3/epi_set/EPI_SET_20220616yx?main=true")
+    main_soup = BeautifulSoup(main_page.content, "html.parser")
+    set_divs = main_soup.find_all("div", {"class":"accpack-head"})
 
-        URL = "https://epicov.org/epi3/epi_set/EPI_SET_20220616yx?detail=" + elem
-        #test = "https://epicov.org/epi3/epi_set/EPI_SET_20220616yx?detail=EPI_ISL_452634/e3470fbb68c543069cb52d22ad0c1dd8"
+    for set_div in set_divs:
+        onclick_func = set_div['onclick']
+        set_url = onclick_func.split('\'')[1]
+        sample_ids = requests.get(set_url)
 
-        page = requests.get(URL)
+        for sample_id in sample_ids.json()["ids"]:
+            time.sleep(1)
 
-        soup = BeautifulSoup(page.content, "html.parser")
-        datadiv = soup.find("div", id="popup-content")
-        children = datadiv.findChildren("div")
+            URL = "https://epicov.org/epi3/epi_set/EPI_SET_20220616yx?detail=" + sample_id
+            #test = "https://epicov.org/epi3/epi_set/EPI_SET_20220616yx?detail=EPI_ISL_452634/e3470fbb68c543069cb52d22ad0c1dd8"
 
-        try:
-            line = children[0].text + ";" + \
-                children[1].text.split(": ")[1] + ";" + \
-                children[2].text.split(": ")[1] + ";" + \
-                children[3].text.split(": ")[1] + ";" + \
-                children[4].text.split(": ")[1] + ";" + \
-                children[5].text.split(": ")[1] + '\n'
+            page = requests.get(URL)
 
-            #print(line)
-            the_file.write(line)
-        except:
-            print("Error while processing " + elem)
+            soup = BeautifulSoup(page.content, "html.parser")
+            datadiv = soup.find("div", id="popup-content")
+            children = datadiv.findChildren("div")
+
+            try:
+                line = children[0].text + ";" + \
+                    children[1].text.split(": ")[1] + ";" + \
+                    children[2].text.split(": ")[1] + ";" + \
+                    children[3].text.split(": ")[1] + ";" + \
+                    children[4].text.split(": ")[1] + ";" + \
+                    children[5].text.split(": ")[1] + '\n'
+
+                #print(line)
+                the_file.write(line)
+            except:
+                print("Error while processing " + sample_id)
 
 
